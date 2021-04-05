bb4ae425bf478543d069df95cde7bf8e51026d58
AntonAntonich <anton.gitgir@google.com>
2021-04-05 18:36:40 +0300

    commit second

--- src/by/anton/array/validator/EmptyStringValidator.java
+++ src/by/anton/array/validator/EmptyStringValidator.java
@@ -2,7 +2,7 @@ package by.anton.array.validator;
 
 public class EmptyStringValidator {
     public static boolean isEmptyString(String string){
-        boolean result = (string == null) || (string.length() == 0);
+        boolean result = (string == null) || (string.isEmpty());
             return result;
     }
 }
